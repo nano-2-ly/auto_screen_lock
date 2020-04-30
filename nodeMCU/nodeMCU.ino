b34e3e5f8a73302fc5604458897837b5ed9fdb23
nano-2-ly <dong991207@unist.ac.kr>
2020-04-30 14:30:29 +0900

    python, nodemcu upgrade

--- nodeMCU/nodeMCU.ino
+++ nodeMCU/nodeMCU.ino
@@ -1,37 +1,75 @@
-const int trigPin = 5;    //Trig 핀 할당
-const int echoPin = 4;    //Echo 핀 할당
+#include "Adafruit_VL53L0X.h"
+#include <ESP8266WiFi.h>
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK  ""
+#endif
  
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+ 
+const char* host = "192.168.x.x";
+const uint16_t port = 8001;
+
 void setup()
 {
-    Serial.begin(115200);    //시리얼 초기화
-    Serial.println("Hello Arduino!!");
+Serial.begin(115200);
  
-    pinMode(trigPin, OUTPUT);    //Trig 핀 output으로 세팅
-    pinMode(echoPin, INPUT);    //Echo 핀 input으로 세팅
+// wait until serial port opens for native USB devices
+while (! Serial)
+{
+delay(1);
+}
+ 
+Serial.println("Adafruit VL53L0X test");
+if (!lox.begin())
+{
+Serial.println(F("Failed to boot VL53L0X"));
+while(1);
+}
+// power
+Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
+WiFi.mode(WIFI_STA);
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+}
+
 }
  
 void loop()
 {
-    long duration, distance;    //기본 변수 선언
- 
-    //Trig 핀으로 10us의 pulse 발생
-    digitalWrite(trigPin, LOW);        //Trig 핀 Low
-    delayMicroseconds(2);            //2us 유지
-    digitalWrite(trigPin, HIGH);    //Trig 핀 High
-    delayMicroseconds(10);            //10us 유지
-    digitalWrite(trigPin, LOW);        //Trig 핀 Low
- 
-    //Echo 핀으로 들어오는 펄스의 시간 측정
-    duration = pulseIn(echoPin, HIGH);        //pulseIn함수가 호출되고 펄스가 입력될 때까지의 시간. us단위로 값을 리턴.
- 
-    //음파가 반사된 시간을 거리로 환산
-    //음파의 속도는 340m/s 이므로 1cm를 이동하는데 약 29us.
-    //따라서, 음파의 이동거리 = 왕복시간 / 1cm 이동 시간 / 2 이다.
-    distance = duration / 29 / 2;        //센치미터로 환산
- 
-    Serial.print(distance);
-    Serial.print("cm");
-    Serial.println();
- 
-    delay(100);
+  VL53L0X_RangingMeasurementData_t measure;
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+  if (client.connected()) {
+    Serial.print("Reading a measurement... ");
+    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+     
+    if (measure.RangeStatus != 4)
+    { // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+    }
+    else
+    {
+    Serial.println(" out of range ");
+    }
+     
+    delay(100);  
+    
+    
+    client.println(measure.RangeMilliMeter);
+  }
+   
+  
 }
